3154203df27fbce5b3f6044eba0982276e27d828
mo3366 <momen23012694@gmail.com>
2026-08-12 22:15:49 +0300

    Question 02

--- C#Basics05/Book.cs
+++ C#Basics05/Book.cs
@@ -7,5 +7,6 @@ namespace C_Basics05
     internal class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
     }
 }
--- C#Basics05/Program.cs
+++ C#Basics05/Program.cs
@@ -6,10 +6,18 @@
         {
             #region Question 01
             //Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why?
-            
+
             //Book book = new Book();
             //Console.WriteLine(book.password);
             // No compilation will happen because private can be accessed inside the same class only
+            #endregion
+
+            #region Question 02
+            //Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why ?
+
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock);
+            //compilation will happen because internal can be accessed from anywhere inside the same project 
             #endregion
         }
     }
