4c21c6bd166e745a083ce441b5baedd6d79cb804
mo3366 <momen23012694@gmail.com>
2026-08-12 22:27:19 +0300

    Question 06

--- C#Basics05/Program.cs
+++ C#Basics05/Program.cs
@@ -44,6 +44,15 @@
             Console.WriteLine((int)Genre.NonFiction);
             Console.WriteLine((int)Genre.Science);
             #endregion
+
+            #region Question 06
+            //Given int genreNumber = 1;, cast it into a Genre value and print the result.
+
+            //int genreNumber = 1;
+            //Genre genre = (Genre)genreNumber;
+            //Console.WriteLine(genre);
+
+            #endregion
         }
     }
 }
