97142719b8887cb1f96657d1892e6ec79efdb5f8
mo3366 <momen23012694@gmail.com>
2026-08-12 22:12:50 +0300

    Question 01

---
+++ C#Basics05/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Basics05
+{
+    internal class Book
+    {
+        private string password = "secret";
+    }
+}
--- C#Basics05/Program.cs
+++ C#Basics05/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Question 01
+            //Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why?
+            
+            //Book book = new Book();
+            //Console.WriteLine(book.password);
+            // No compilation will happen because private can be accessed inside the same class only
+            #endregion
         }
     }
 }
